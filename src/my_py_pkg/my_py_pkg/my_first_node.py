d927d7f8b113659178a099ddee9349510f15eb2b
Allan Presotto <allanpresottoifpr@gmail.com>
2023-11-02 22:26:10 -0300

    upgrade node with oop

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -3,14 +3,23 @@
 This is a simple node that publishes a string message to the topic "my_first_topic" 
 """  # noqa
 
+from typing import List, Optional
+
 import rclpy
+from rclpy.context import Context
 from rclpy.node import Node
+from rclpy.parameter import Parameter
+
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello ROS2 with OOP!")
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test_node")
-    node.get_logger().info("Hello ROS2!")
+    node = MyNode()
     rclpy.spin(node)  # mantém o programa rodando
     rclpy.shutdown()
 
